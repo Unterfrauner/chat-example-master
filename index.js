20c702f11e307d6c3f14cd8b9d962f5cb0322279
Unterfrauner <florianunterfrauner3@gmail.com>
2022-04-16 16:37:55 +0200

    es sollte gehen

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000; //der port gibt an wie man den server erreicht
-const verbunden = "verbunden";
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-    res.sendFile('https://15-puzzle-spiel.netlify.app/'); //??????geht das so??
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => { //sobald man mit server verbunden ist
-    io.emit('verbindung', verbunden)
-    socket.on('chat message', msg => { //sobald nachricht verschickt wurde()chat message
-        io.emit('chat message', msg);
-    });
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(port, () => {
-    console.log(`Socket.IO server running at ${port}/`); //welcher server????
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
-
