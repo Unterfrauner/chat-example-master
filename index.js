b8373c6733dccb78afb9842195d49a02342dde34
Unterfrauner <florianunterfrauner3@gmail.com>
2022-04-16 15:45:49 +0200

    etwas geändert

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io');
-const port = process.env.PORT || 3000;
-
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000; //der port gibt an wie man den server erreicht
+const verbunden = "verbunden";
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/frontend/index.html');
 });
 
 io.on('connection', (socket) => { //sobald man mit server verbunden ist
+    io.emit('verbindung', verbunden)
     socket.on('chat message', msg => { //sobald nachricht verschickt wurde()chat message
         io.emit('chat message', msg);
     });
 });
 
 http.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`); //welcher server????
+    console.log(`Socket.IO server running at ${port}/`); //welcher server????
 });
+
